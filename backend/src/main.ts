db3e805f8c362dc485371df326a8147f4bfbc840
viniciuscoassis <vinicius.coassis@gmail.com>
2024-10-20 13:22:40 -0300

    feat: add nest config and validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log', 'debug', 'verbose'],
   });
+  
+  /**
+   * Global Prefix
+   * This is used to prefix the routes with the global prefix
+   * This is useful for the frontend to know where the API is located
+   * All routes will be prefixed with /api
+  */
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  /**
+   * Global Validation Pipe
+   * This is used to validate the body of the request
+   * It will throw an error if the body is not valid
+   */
+  app.useGlobalPipes(new ValidationPipe());
+
+  /**
+   * Global Port
+   * This is used to get the port from the environment variables
+   * If no port is found, it will use the default port 3000
+   */
   const port = process.env.PORT || 3000;
+  
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
