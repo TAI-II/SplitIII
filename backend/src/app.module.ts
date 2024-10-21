ffafddf00590ae839d1a02271c4452a65e45513a
Vinicius Assis <106977892+viniciuscoassis@users.noreply.github.com>
2024-10-20 21:27:35 -0300

    feat: init openai integration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,11 @@ import { OpenaiModule } from './modules/openai/openai.module';
 import { SessionsModule } from './modules/sessions/sessions.module';
 import { UserModule } from './modules/users/user.module';
 import { TabsModule } from './modules/tabs/tabs.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(
       'mongodb+srv://viniciusassis:KUm2RyLvJ9tGQOds@cluster0.6aj3k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     ),
