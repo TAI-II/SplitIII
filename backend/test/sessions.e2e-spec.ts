d8db9a8840593fc289921880ad349ddfd73d2c0d
Vinicius Assis <106977892+viniciuscoassis@users.noreply.github.com>
2024-11-02 19:39:14 -0300

    feat: add cleanup feature for e2e testing

--- backend/test/sessions.e2e-spec.ts
+++ backend/test/sessions.e2e-spec.ts
@@ -1,30 +1,30 @@
+import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication, ValidationPipe } from '@nestjs/common';
-import { io, Socket } from 'socket.io-client';
-import { AppModule } from '../src/app.module';
-import { ITab } from '../src/modules/tabs/interface/tab.interface';
 import * as request from 'supertest';
-import { Types } from 'mongoose';
-import { SessionsService } from '../src/modules/sessions/sessions.service';
+import { AppModule } from '../src/app.module';
+import { SessionsService } from 'src/modules/sessions/sessions.service';
+import { UserService } from 'src/modules/users/user.service';
+import { CleanupUtility } from './utility/cleanup.utility';
 
 describe('Sessions Flow (e2e)', () => {
   let app: INestApplication;
-  let sessionId: string;
-  let userId: Types.ObjectId;
-  let sessionService = { findAll: () => ['test'] };
+  let sessionsService: SessionsService;
+  let userService: UserService;
 
   beforeAll(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     })
       .compile();
+    sessionsService = moduleRef.get<SessionsService>(SessionsService);
+    userService = moduleRef.get<UserService>(UserService);
 
     app = moduleRef.createNestApplication();
 
     await app.init();
   });
 
-  it.only('should create a new session', async () => {
+  it('should create a new session', async () => {
     const response = await request(app.getHttpServer())
       .post('/sessions')
       .send({
@@ -44,12 +44,54 @@ describe('Sessions Flow (e2e)', () => {
     };
     expect(response.status).toBe(201);
     expect(response.body).toEqual(expectedBody);
+    CleanupUtility.trackSessionId(response.body._id);
+    CleanupUtility.trackUserId(response.body.creatorId);
+  });
 
-    sessionId = response.body._id;
+  it('should create a user', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/users')
+      .send({ name: 'Test User' });
+
+    const expectedBody = {
+      __v: 0,
+      _id: expect.any(String),
+      name: 'Test User',
+    };
+    expect(response.status).toBe(201);
+    expect(response.body).toEqual(expectedBody);
+  });
+
+  it('should create session with existing user as creator', async () => {
+    const responseUser = await request(app.getHttpServer())
+      .post('/users')
+      .send({ name: 'Test User' });
+
+    const response = await request(app.getHttpServer())
+      .post('/sessions')
+      .send({
+        name: 'Test Session',
+        creatorId: responseUser.body._id,
+      });
+
+    const expectedBody = {
+      __v: 0,
+      _id: expect.any(String),
+      name: 'Test Session',
+      code: expect.any(String),
+      creatorId: responseUser.body._id,
+      sessionUsers: [],
+      createdAt: expect.any(String),
+      updatedAt: expect.any(String),
+    };
+    expect(response.status).toBe(201);
+    expect(response.body).toEqual(expectedBody);
+    CleanupUtility.trackSessionId(response.body._id);
+    CleanupUtility.trackUserId(responseUser.body._id);
   });
 
   afterAll(async () => {
-    // socketClient.close();
     await app.close();
+    await CleanupUtility.cleanup(sessionsService, userService);
   });
 });
